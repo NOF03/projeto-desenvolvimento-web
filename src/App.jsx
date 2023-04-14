5638b6073305e3e40df89726cf6a16af6d1b451d
NOF03 <105490848+NOF03@users.noreply.github.com>
2023-04-14 15:29:57 +0100

    Added module

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import { ReactComponent as Logo } from './assets/logo.svg'
+
 
 export default function App() {
+
   return (
+
     <>
-      <Container>
+
+      <Container className='text-center'>
         <Row>
           <Col>1 of 4</Col>
+          <Logo />
           <Col>2 of 4</Col>
           <Col>3 of 4</Col>
           <Col>4 of 4</Col>
         </Row>
       </Container>
+
+
+      <style>
+        {`
+    .container {
+      background-color: purple;
+      color: white;
+    }
+
+    .btn-xxl {
+      padding: 1rem 1.5rem;
+      font-size: 1.5rem;
+    }
+    `}
+      </style>
     </>
+
+
   );
+
 };
+
