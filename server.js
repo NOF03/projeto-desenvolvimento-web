b47ca91f9caab3790adf571c602f6ce8868a5282
NOF03 <105490848+NOF03@users.noreply.github.com>
2023-05-07 21:12:58 +0100

    cxvxc

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-
+const PORT = 3001;
+/**
+ * Aqui vamos fazemos referencias de todas as rotas
+ */
 let userRoute = require("./src/routes/userRoute");
 
 app.use(express.json()); //Transforma JSON para objecto!
 app.use(cors());
 
+//Mongoose connection
 mongoose
   .connect(
     "mongodb+srv://nof:AcgI3ykTIK37DVO7@fragmer.vgvqf8v.mongodb.net/fragmer?retryWrites=true&w=majority",
@@ -19,8 +23,9 @@ mongoose
   .then(() => console.log("connected"))
   .catch((err) => console.log(err));
 
+//Middleware que é executado sempre que fazemos, por exemplo, um get/post request.
 app.use("/auth", userRoute);
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
   console.log("SERVER RUNS PERFECTLY!");
 });
